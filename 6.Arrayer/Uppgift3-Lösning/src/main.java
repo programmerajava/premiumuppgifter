1146eeae26f9e27c2674ac5dc7805d23167ba207
Gustav Andersson <gustavandersson1994@gmail.com>
2020-04-28 20:58:16 +0200

    Finalize assignment 3

--- 6.Arrayer/Uppgift3-Lösning/src/main.java
+++ 6.Arrayer/Uppgift3-Lösning/src/main.java
@@ -28,13 +28,16 @@ public class main {
         System.out.println("Storleken på arraylisten är: " + getSize(timestamps));
 
         // Uppgift 3
+        double maxTemp = getMaxValueInArraylist(temp);
         System.out.println("Den högsta temperaturen som uppmättes under 2019 var: " +
-                            String.format("%.2f", getMaxTemp(temp)) +
+                            String.format("%.2f", maxTemp) +
                             "° C");
 
+
         // Uppgift 4
-        int maxInd = getIndOfMaxTemp(temp);
+        int maxInd = getIndOfVal(temp, maxTemp);
         String maxTempTimestamp = timestamps.get(maxInd);
+
 
         System.out.println("Timestampet då var: " + maxTempTimestamp +
                            " vilket motsvarar: " + getDateByTimestamp(maxTempTimestamp));
@@ -43,37 +46,94 @@ public class main {
         // Uppgift 5
         System.out.println("Totalt regnade det: " + String.format("%.2f", getRainSum(rain)) +  " mm under 2019");
 
+        // Uppgift 6
+        double minBarometer = getMinValueInArraylist(barometer);
+        int minBarometerInd = getIndOfVal(barometer, minBarometer);
+        String minBarometerTimestamp = timestamps.get(minBarometerInd);
+
+        System.out.println("Det lägsta uppmätta lufttrycket var: " + minBarometer +
+                           ", timestampet då var: " + minBarometerTimestamp +
+                           ", vilket motsvarar: " + getDateByTimestamp(minBarometerTimestamp));
+
+
+        // Uppgift 7
+        System.out.println("Medeltemperaturen under 2019 var: " +  String.format("%.2f", getMeanTemp(temp)) + "° C");
+
+
 
     }
 
 
+    /**
+     * Returnera storleken på arraylisten av godtycklig datatyp.
+     * ArrayList<?> betyder att vi inte bryr oss om vilken datatyp vi har i Arraylisten.
+     * Denna metod kommer alltså fungera för alla arraylists, oavset datatyp
+     * Returnerar längden av arraylisten i form av en int
+     */
     private static int getSize(ArrayList<?> data){
         return data.size();
     }
 
 
-    private static Double getMaxTemp(ArrayList<Double> temps){
+    /**
+    * Input: En arraylist av datatypen double
+    * Returnerar: Maxvärdet i arraylisten i form av en double
+    * Stegar igenom alla värden i arraylisten och sparar undan det värde som är störst.
+    * Observera att denna metod utgår från att det endast finns ett högsta värde!
+    */
+    private static Double getMaxValueInArraylist(ArrayList<Double> arr){
 
-        double maxTemp = -100.0;
-        for(int i = 0; i < temps.size(); i++){
-            if(temps.get(i) > maxTemp){
-                maxTemp = temps.get(i);
+        double maxVal= -100000.0;
+        for(int i = 0; i < arr.size(); i++){
+            if(arr.get(i) > maxVal){
+                maxVal = arr.get(i);
             }
         }
-        return maxTemp;
-    }
-
-    private static int getIndOfMaxTemp(ArrayList<Double> temps){
-        return temps.indexOf(getMaxTemp(temps));
+        return maxVal;
     }
 
 
-    // Konverterar ett timestamp till ett datum
-    private static java.util.Date getDateByTimestamp(String maxTempTimestamp){
-        return new java.util.Date((long)Long.parseLong(maxTempTimestamp)*1000);
+    /**
+    * Input: En arraylist av datatypen double
+    * Returnerar: Minsta värdet i arraylisten i form av en double
+    * Stegar igenom alla värden i arraylisten och sparar undan det värde som är minst.
+    * Observera att denna metod utgår från att det endast finns ett minsta värde!
+    */
+    private static double getMinValueInArraylist(ArrayList<Double> arr){
+        double minVal = 1000000;
+        for(int i = 0; i < arr.size(); i++){
+            if(arr.get(i) < minVal){
+                minVal = arr.get(i);
+            }
+        }
+        return minVal;
+    }
+
+    /**
+     * arr: en arraylist bestående av doubles
+     * val: ett värde som finns i arraylisten
+     * Returnerar indexet där värdet ligger i arraylisten
+     * Observera att metoden utgår från att värdet finns i arraylisten
+     */
+    private static int getIndOfVal(ArrayList<Double> arr, double val){
+        return arr.indexOf(val);
     }
 
 
+    /**
+    * Input: Ett unix-timestamp av datatypen String
+    * Returnerar: Ett datum i av datatypen java.util.Date
+    */
+    private static java.util.Date getDateByTimestamp(String timestamp){
+        return new java.util.Date((long)Long.parseLong(timestamp)*1000);
+    }
+
+
+    /**
+    * Summerar alla värden i arraylisten
+    * Input: En Arraylist bestående av doubles.
+    * Returnerar: Summan av alla värden i arraylisten
+    * */
     private static double getRainSum(ArrayList<Double> rain){
         double rainSum = 0;
         for(int i = 0; i < rain.size(); i++){
@@ -81,6 +141,23 @@ public class main {
         }
         return rainSum;
     }
+
+
+    /**
+     * Summerar alla värden i arraylisten och returnerar medelvärdet av arraylisten.
+     * Input: En arraylisten bestående av doubles
+     * Returnerar: Medelvärdet av alla värden i arraylisten
+     */
+    private static double getMeanTemp(ArrayList<Double> arr){
+        double sum = 0;
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr.get(i);
+        }
+
+        return sum/(arr.size());
+    }
+
+
 
 
 
